7ac7ae751e0906bc442d0ac9b3f93b513a0a3043
Azult <perlovia@gmail.com>
2019-12-30 20:26:29 +0200

    new files

--- Containers/unclosed-file/create.sh
+++ Containers/unclosed-file/create.sh
@@ -19,7 +19,7 @@ else
 	echo -e 'Aa123456\nAa123456\n' | passwd $user
 	cp .$(echo $0|cut -d '.' -f2)$fileName.c /rootfs/home/$user/$fileName.c
 	sed -i "1i#define USERID $(id -u $user)" /rootfs/home/$user/$fileName.c
-	gcc /rootfs/home/$user/$fileName.c -o /rootfs/home/$user/$fileName && chmod u+s /rootfs/home/$user/$fileName
+	gcc -w /rootfs/home/$user/$fileName.c -o /rootfs/home/$user/$fileName && chmod u+s /rootfs/home/$user/$fileName
 	tail -n 1 /etc/passwd $1 | while read x; do echo "${x///rootfs/}"; done >> /rootfs/etc/passwd
 fi
 
