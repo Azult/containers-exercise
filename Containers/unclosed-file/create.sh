ca46d81284d8ff55772af2636e4e13301ab82144
Azult <perlovia@gmail.com>
2019-12-31 11:34:04 +0200

    new files

--- Containers/unclosed-file/create.sh
+++ Containers/unclosed-file/create.sh
@@ -9,17 +9,25 @@ echo "[*] Creating 'unclosed-file' container"
 
 user="unclosed"
 fileName="unclosed-file" #Without .c
+password="Aa123456"
 
 if [ -d "/rootfs/home/$user" ] 
 then
-	echo "[-] 'unclosed-file' container already exist"
+	echo "[-] '$user' container already exist"
 else
+	# Create container home directory
 	mkdir /rootfs/home/$user
+	# Create user with shell and directory
 	useradd -s /rootfs/home/$user/$fileName -d /rootfs/home/$user $user
-	echo -e 'Aa123456\nAa123456\n' | passwd $user
+	# Set password
+	echo -e '$password\n$password\n' | passwd $user
+	# Compile the shell
 	cp .$(echo $0|cut -d '.' -f2)$fileName.c /rootfs/home/$user/$fileName.c
 	sed -i "1i#define USERID $(id -u $user)" /rootfs/home/$user/$fileName.c
-	gcc -w /rootfs/home/$user/$fileName.c -o /rootfs/home/$user/$fileName && chmod u+s /rootfs/home/$user/$fileName
+	gcc -w /rootfs/home/$user/$fileName.c -o /rootfs/home/$user/$fileName
+	#&& chmod u+s /rootfs/home/$user/$fileName
+
+	#Update the rootfs passwd
 	tail -n 1 /etc/passwd $1 | while read x; do echo "${x///rootfs/}"; done >> /rootfs/etc/passwd
 fi
 
